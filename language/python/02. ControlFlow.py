a3bdb4b610a22a42aa190b75a69abc560ee7dedf
Shrewd <kimkjjin@gmail.com>
2021-03-30 13:50:01 +0900

    Update 02. ControlFlow.py

--- language/python/02. ControlFlow.py
+++ language/python/02. ControlFlow.py
@@ -1,1 +1,29 @@
- 1. IF
+ # 2021-03-30
+ # ControlFlow.py
+ # 제어문
+ 
+1. If
+ if 조건:
+   do1
+ elsif 조건:
+   do2
+ else 조건:
+   do3
+   
+ - 들여쓰기 (spacebar 4개 권장)
+ - pass
+  
+2. While
+ while 조건:
+   do
+   
+ - break
+ - continue
+ 
+3. For
+ for 변수 in ():
+   do
+ 
+ - range(a,b): a이상 b미만의 숫자들
+  
+ 
